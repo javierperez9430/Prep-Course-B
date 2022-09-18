fdecce43a50d8388ecab6621fe1f33ab16abe932
unknown <jpeduardo30@gmail.comm>
2022-09-18 13:05:24 -0500

    HW 01 solution

---
+++ 02-JS-II/homework/homework.js
@@ -0,0 +1,170 @@
+// En los próximos 22 problemas, deberás completar la función.
+// Todo tu código irá dentro de las llaves de la función.
+// Asegúrate que usas "return" cuando la consola te lo pida.
+// Pista: "console.log()" NO fucionará.
+// No cambies los nombres de las funciones.
+
+function devolverString(str) {
+    return (str)
+    
+  }
+  
+  function suma(x, y) {
+    return (x+y)
+  }
+  
+  function resta(x, y) {
+    return (x-y)
+  }
+  
+  function multiplica(x, y) {
+    return (x*y)
+  }
+  
+  function divide(x, y) {
+    return (x/y)
+  }
+  
+  function sonIguales(x, y) {
+    if (x == y){
+      return true
+    }else
+    return false
+  }
+  
+  function tienenMismaLongitud(str1, str2) {
+    if (str1.length == str2.length){
+      return true
+    }else
+    return false
+  }
+  
+  function menosQueNoventa(num) {
+    if (num < 90){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  function mayorQueCincuenta(num) {
+    if (num > 50){
+      return true
+     }else{
+      return false
+     }
+  }
+  
+  function obtenerResto(x, y) {
+    return (x%y)
+  }
+  
+  function esPar(num) {
+    if(num %2 == 0){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  function esImpar(num) {
+    if(num %2 == 1){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  function elevarAlCuadrado(num) {
+    return (num**2)
+  }
+  
+  function elevarAlCubo(num) {
+    return (num**3)
+  }
+  
+  function elevar(num, exponent) {
+    return(num**exponent)
+  }
+  
+  function redondearNumero(num) {
+    return Math.round(num)
+  }
+  
+  function redondearHaciaArriba(num) {
+    return Math.ceil(num,1)
+  }
+  
+  function numeroRandom() {
+    return Math.random(1)
+  }
+  
+  function esPositivo(numero) {
+    if (numero==0){
+      return false
+    }else if(numero < 0){
+      return "Es negativo"
+    }else if(numero > 0){
+      return "Es positivo"
+    }
+  }
+  
+  function agregarSimboloExclamacion(str) {
+    return `${str}!`
+  }
+  
+  function combinarNombres(nombre, apellido) {
+    return nombre + " " + apellido
+  }
+  
+  function obtenerSaludo(nombre) {
+    return "Hola" + " " + nombre + "!"
+  }
+  
+  function obtenerAreaRectangulo(alto, ancho) {
+    return alto*ancho
+  }
+  
+  function retornarPerimetro(lado) {
+    return lado*4
+  }
+  
+  function areaDelTriangulo(base, altura) {
+    return base*altura / 2
+  }
+  
+  function deEuroAdolar(euro) {
+    return euro*1.20
+  }
+  
+  // No modificar nada debajo de esta línea
+  // --------------------------------
+  
+  module.exports = {
+    devolverString,
+    tienenMismaLongitud,
+    sonIguales,
+    menosQueNoventa,
+    mayorQueCincuenta,
+    suma,
+    resta,
+    divide,
+    multiplica,
+    obtenerResto,
+    esPar,
+    esImpar,
+    elevarAlCuadrado,
+    elevarAlCubo,
+    elevar,
+    redondearNumero,
+    redondearHaciaArriba,
+    numeroRandom,
+    esPositivo,
+    agregarSimboloExclamacion,
+    combinarNombres,
+    obtenerSaludo,
+    obtenerAreaRectangulo,
+    retornarPerimetro,
+    areaDelTriangulo,
+    deEuroAdolar,
+  };
